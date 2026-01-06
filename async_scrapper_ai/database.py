b44b4654ba9f9ccf184cc7fdd0ce809ebb2addd6
ujjwal-basnet <ujjwalvictus15@gmail.com>
2026-01-07 02:03:21 +0545

    comment:  get db

---
+++ async_scrapper_ai/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import declarative_base, sessionmaker
+from settings import settings
+from loguru import logger 
+
+logger.info("extracting database url from settings")
+DATABASE_URL= str(settings.pg_dsn)
+
+logger.info("creating engine and session local")
+engine= create_engine(DATABASE_URL , echo= False)
+SessionLocal= sessionmaker(bind= engine, autocommit= False , autoflash= False)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
